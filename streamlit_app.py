47ca6d3119ebe34f0da83a19d2a7b33892e61d06
Boc86 <bocest1986@gmail.com>
2024-12-03 13:52:41 +0000

    more pages added

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,63 +10,100 @@ st.set_page_config(
 )
 
 st.logo(
-    icon,
+    image=Image.open('images/logo.png'),
     link="https://www.kingswoodrfc.co.uk",
-    size="large"
+    size="large",
+    icon_image=Image.open('images/icon.png')
 )
 
 ### Home ###
 home = st.Page(
-    "layout/Home/1 Home.py", title="Home", icon="🏡", default= True
+    "layout/Home/1 Home.py", title="Home", icon=":material/home:", default= True
 )
 
 how_to = st.Page(
-    "layout/Home/2 HowTo.py", title="How to use the Kingswood RFC Volunteer Community Web App", icon="❓"
+    "layout/Home/2 HowTo.py", title="How to use the Kingswood RFC Volunteer Community Web App", icon=":material/help:"
 )
 
 ### Parent Info ###
 parent_info = st.Page(
-    "layout/Parent Info/1 Parent Info.py", title="Parent Info", icon="ℹ️"
+    "layout/Parent Info/1 Parent Info.py", title="Welcome Letter", icon=":material/info:"
+)
+
+parent_volunteering = st.Page(
+    "layout/Parent Info/2 Parent Volunteering.py", title="Volunteering", icon=":material/volunteer_activism:"
+)
+
+parent_age_groups = st.Page(
+    "layout/Parent Info/3 parent Age Groups.py", title="Age Groups", icon=":material/pin:"
 )
 
 ### Player Info ###
 player_info = st.Page(
-    "layout/Player Info/1 Player Info.py", title="Player Info", icon="ℹ️"
+    "layout/Player Info/1 Player Info.py", title="Player Info", icon=":material/info:"
 )
 
 ### Policies ###
 policies = st.Page(
-    "layout/Policies/1 Policies.py", title="Policies", icon="📁"
+    "layout/Policies/1 Policies.py", title="Policies", icon=":material/topic:"
 )
 
 ### Safeguarding ###
 safeguarding = st.Page(
-    "layout/Safeguarding/1 Safeguarding.py", title="Safeguarding", icon="👷"
+    "layout/Safeguarding/1 Safeguarding.py", title="Safeguarding", icon=":material/enhanced_encryption:"
+)
+
+safeguarding_officers = st.Page(
+    "layout/Safeguarding/2 Safeguarding Officers.py", title="Safeguarding Officers", icon=":material/group:"
+)
+
+safeguarding_ploicy = st.Page(
+    "layout/Safeguarding/3 Safeguarding Policy.py", title="Safeguarding Policy", icon=":material/description:"
 )
 
 ### Coaching ###
-coaching = st.Page(
-    "layout/Coaching/1 Coaching.py", title="Coaching", icon="🚌"
+coaches_handbook = st.Page(
+    "layout/Coaching/1 Coaches Handbook.py", title="Coaches Handbook", icon=":material/hub:"
 )
 coaching_team = st.Page(
-    "layout/Coaching/2 Coaching Team.py", title="Coaching Team", icon="🤼"
+    "layout/Coaching/2 Coaching Team.py", title="Coaching Team", icon=":material/diversity_2:"
+)
+
+coaching_juniors = st.Page(
+    "layout/Coaching/3 Juniors.py", title="Juniors", icon=":material/man:"
+)
+
+coaching_athenas = st.Page(
+    "layout/Coaching/4 Athenas.py", title="Athenas", icon=":material/woman:"
+)
+
+coaching_minis = st.Page(
+    "layout/Coaching/5 Minis.py", title="Minis", icon=":material/boy:"
+)
+
+coaching_resources = st.Page(
+    "layout/Coaching/6 Resources.py", title="Resources", icon=":material/share:"
 )
 
 ### Mental Health ###
 mental_health = st.Page(
-    "layout/Mental Health/1 Mental Health.py", title="Mental Health", icon="⛑️"
+    "layout/Mental Health/1 Mental Health.py", title="Mental Health", icon=":material/health_and_safety:"
+)
+
+mental_health_resources = st.Page(
+    "layout/Mental Health/2 Resources and Support.py", title="Resources and Support", icon=":material/link:"
 )
 
 pg = st.navigation(
     {
         "Home": [home, how_to],
-        "Parent Info": [parent_info],
+        "Parent Info": [parent_info, parent_volunteering, parent_age_groups],
         "Player Info": [player_info],
         "Policies": [policies],
-        "Safeguarding": [safeguarding],
-        "Coaching": [coaching, coaching_team],
-        "Mental Health": [mental_health]
+        "Safeguarding": [safeguarding, safeguarding_officers, safeguarding_ploicy],
+        "Coaching": [coaches_handbook, coaching_team, coaching_juniors, coaching_athenas, coaching_minis, coaching_resources],
+        "Mental Health": [mental_health, mental_health_resources]
     }
 )
 
-pg.run()
+pg.run() 
