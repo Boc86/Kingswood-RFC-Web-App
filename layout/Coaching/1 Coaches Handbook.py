6f79fa0749f1933784f9615efe11ac0600a81f7d
Boc86 <bocest1986@gmail.com>
2024-12-03 14:32:47 +0000

    Links fixed

--- layout/Coaching/1 Coaches Handbook.py
+++ layout/Coaching/1 Coaches Handbook.py
@@ -8,8 +8,8 @@ with st.container():
     st.write("---")
 
     st.subheader("Usefull Links")
-    st.page_link("layout/coaching/2 Coaching Team.py", label=None, icon=":material/diversity_2:", help=None, disabled=False, use_container_width=None)
-    st.page_link("layout/coaching/3 Juniors.py", label=None, icon=":material/man:", help=None, disabled=False, use_container_width=None)
-    st.page_link("layout/coaching/4 Athenas.py", label=None, icon=":material/woman:", help=None, disabled=False, use_container_width=None)
-    st.page_link("layout/coaching/5 Minis.py", label=None, icon=":material/boy:", help=None, disabled=False, use_container_width=None)
-    st.page_link("layout/coaching/6 Resources.py", label=None, icon=":material/share:", help=None, disabled=False, use_container_width=None)
+    st.page_link("layout/Coaching/2 Coaching Team.py", label=None, icon=":material/diversity_2:", help=None, disabled=False, use_container_width=None)
+    st.page_link("layout/Coaching/3 Juniors.py", label=None, icon=":material/man:", help=None, disabled=False, use_container_width=None)
+    st.page_link("layout/Coaching/4 Athenas.py", label=None, icon=":material/woman:", help=None, disabled=False, use_container_width=None)
+    st.page_link("layout/Coaching/5 Minis.py", label=None, icon=":material/boy:", help=None, disabled=False, use_container_width=None)
+    st.page_link("layout/Coaching/6 Resources.py", label=None, icon=":material/share:", help=None, disabled=False, use_container_width=None)
