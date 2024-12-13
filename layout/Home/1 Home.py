4cabec363273a1aa3011d226c36696c8d582621d
Boc <bocest1986@gmail.com>
2024-12-13 08:57:45 +0000

    added link to key personnel

--- layout/Home/1 Home.py
+++ layout/Home/1 Home.py
@@ -32,6 +32,7 @@ with st.container():
         st.page_link("layout/Home/2 HowTo.py", label=None, icon=":material/help:", help=None, disabled=False, use_container_width=None)
         st.page_link("layout/Parent Info/1 Parent Info.py", label=None, icon=":material/info:", help=None, disabled=False, use_container_width=None)
         st.page_link("layout/Player Info/1 Player Info.py", label=None, icon=":material/info:", help=None, disabled=False, use_container_width=None)
+        st.page_link("layout/Home/2.1 Key Personnel.py", label=None, icon=":material/account_circle:", help=None, disabled=False, use_container_width=None)
         st.page_link("layout/Coaching/2 Coaching Team.py", label=None, icon=":material/diversity_2:", help=None, disabled=False, use_container_width=None)
         st.page_link("layout/Safeguarding/1 Safeguarding.py", label=None, icon=":material/enhanced_encryption:", help=None, disabled=False, use_container_width=None)
         st.page_link("layout/Mental Health/1 Mental Health.py", label=None, icon=":material/health_and_safety:", help=None, disabled=False, use_container_width=None)
