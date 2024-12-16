cbf6a332d3d0595f21281e04d2764d11648cfb88
Boc <bocest1986@gmail.com>
2024-12-16 11:26:51 +0000

    updated coaches as not all are paid club members

--- layout/Coaching/2 Coaching Team.py
+++ layout/Coaching/2 Coaching Team.py
@@ -39,33 +39,41 @@ with st.expander("Colts", icon=":material/info:"):
 with st.expander("Under 16's", icon=":material/info:"):
     left_col, right_col = st.columns(2)
     with left_col:
-        st.write("???")
+        st.write("Mark Beresford")
+        st.write("Ian Starr")
+        st.write("Jim Hacker")
     with right_col:
-        st.write("???")
+        st.write("Dave Fairweather")
+        st.write("Archie Honeyfield")
 
 # Under 15's coaches
 with st.expander("Under 15's", icon=":material/info:"):
     left_col, right_col = st.columns(2)
     with left_col:
-        st.write("???")
+        st.write("Dave Hodges")
     with right_col:
-        st.write("???")
+        st.empty
 
 # Under 14's coaches
 with st.expander("Under 14's", icon=":material/info:"):
     left_col, right_col = st.columns(2)
     with left_col:
-        st.write("???")
+        st.write("Mel Hacker")
+        st.write("Oliver Golding")
     with right_col:
-        st.write("???")
+        st.write("Joseph Piper")
+        st.write("George Clark-Dove")
 
 # Under 13's coaches
 with st.expander("Under 13's", icon=":material/info:"):
     left_col, right_col = st.columns(2)
     with left_col:
-        st.write("???")
+        st.write("Joe Foster")
+        st.write("Phil Matisian")
+        st.write("Owain Beecham")
     with right_col:
-        st.write("???")
+        st.write("Matt Stone")
+        st.write("Dan Stone")
 
 ### Athenas Section
 st.subheader("Athenas")
@@ -75,16 +83,20 @@ with st.expander("Under 16s", icon=":material/info:"):
     left_col, right_col = st.columns(2)
     with left_col:
         st.write("Neil Ward")
+        st.write("Gary Sims")
     with right_col:
-        st.write("Gary")
+        st.write("Steve O'Callahan")
+        st.write("Rachael Garding")
 
 # Under 14's coaches
 with st.expander("Under 14's", icon=":material/info:"):
     left_col, right_col = st.columns(2)
     with left_col:
         st.write("Liam O'Laughlon")
+        st.write("Don Smith")
     with right_col:
         st.write("Chris Deane")
+        st.write("Darren Brown")
 
 # Under 12's coaches
 with st.expander("Under 12's", icon=":material/info:"):
@@ -122,24 +134,28 @@ with st.expander("Under 10's", icon=":material/info:"):
     left_col, right_col = st.columns(2)
     with left_col:
         st.write("Chris Broome")
+        st.write("Amy Wheeler")
     with right_col:
-        st.write("James Peters")
+        st.write("James Pearson")
+        st.write("Stafford McLaughlin")
 
 # Under 9's coaches
 with st.expander("Under 9's", icon=":material/info:"):
     left_col, right_col = st.columns(2)
     with left_col:
-        st.write("???")
+        st.write("Jess Hill")
+        st.write("Steve Bennett")
     with right_col:
-        st.write("???")
+        st.write("Craig Williams")
 
 # Under 8's coaches
 with st.expander("Under 8's", icon=":material/info:"):
     left_col, right_col = st.columns(2)
     with left_col:
-        st.write("???")
+        st.write("Lawrence Harrity")
+        st.write("Robbie Edwards")
     with right_col:
-        st.write("???")
+        st.write("Stuart Rogers")
 
 # Under 7's coaches
 with st.expander("Under 7's", icon=":material/info:"):
@@ -147,7 +163,7 @@ with st.expander("Under 7's", icon=":material/info:"):
     with left_col:
         st.write("Mike Gibbs")
     with right_col:
-        st.write("Sophie")
+        st.write("Sophie Jacobs-Wyburn")
 
 # Micros coaches
 with st.expander("Micros", icon=":material/info:"):
