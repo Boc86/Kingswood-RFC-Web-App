2655a0927e6d4abb336dd661d02f2ed7adf672fc
Boc <bocest1986@gmail.com>
2024-12-19 09:49:07 +0000

    moved submit resource button

--- layout/Coaching/6 Resources.py
+++ layout/Coaching/6 Resources.py
@@ -61,6 +61,6 @@ with st.expander("Resources", icon=":material/library_books:"):
             # Add a horizontal divider to separate the resources
             st.write("---")
             i += 1
-        # Add a button to show the dialog box to add a resource
-        st.button("Share a resource", on_click=show_resource_form)
+    # Add a button to show the dialog box to add a resource
+    st.button("Share a resource", on_click=show_resource_form)
 
