bd4abe725987eb579f2fe63db537c6869a9a77d0
Boc <bocest1986@gmail.com>
2025-01-14 14:29:42 +0000

    added labels to some links for clarity

--- layout/Home/1 Home.py
+++ layout/Home/1 Home.py
@@ -30,13 +30,13 @@ with st.container():
         st.subheader("Useful Links")
         # Add page links with icons
         st.page_link("layout/Home/2 HowTo.py", label=None, icon=":material/help:", help=None, disabled=False, use_container_width=None)
-        st.page_link("layout/Parent Info/1 Parent Info.py", label=None, icon=":material/info:", help=None, disabled=False, use_container_width=None)
-        st.page_link("layout/Player Info/1 Player Info.py", label=None, icon=":material/info:", help=None, disabled=False, use_container_width=None)
+        st.page_link("layout/Parent Info/1 Parent Info.py", label="Parent Welcome Letter", icon=":material/info:", help=None, disabled=False, use_container_width=None)
+        st.page_link("layout/Player Info/1 Player Info.py", label="Player Welcome Letter", icon=":material/info:", help=None, disabled=False, use_container_width=None)
         st.page_link("layout/Home/2.1 Key Personnel.py", label=None, icon=":material/account_circle:", help=None, disabled=False, use_container_width=None)
         st.page_link("layout/Coaching/2 Coaching Team.py", label=None, icon=":material/diversity_2:", help=None, disabled=False, use_container_width=None)
         st.page_link("layout/Safeguarding/1 Safeguarding.py", label=None, icon=":material/enhanced_encryption:", help=None, disabled=False, use_container_width=None)
         st.page_link("layout/Mental Health/1 Mental Health.py", label=None, icon=":material/health_and_safety:", help=None, disabled=False, use_container_width=None)
-        st.page_link("layout/Policies/1 Policies.py", label=None, icon=":material/topic:", help=None, disabled=False, use_container_width=None)  
+        st.page_link("layout/Policies/1 Policies.py", label="Club Polices", icon=":material/topic:", help=None, disabled=False, use_container_width=None)  
     
     # In the right column, leave it empty for future use
     with right_col:
